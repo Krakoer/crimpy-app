8cdb326d3dd438d48c1500fdef80b2b88d59ea3f
Debian <debian@ns3025177.ip-149-202-73.eu>
2026-09-20 22:14:30 +0000

    Round 2 fixes: carry the activity label with its icon and put the gold chip on contract

--- lib/views/screens/home_screen/widgets/today_training_card.dart
+++ lib/views/screens/home_screen/widgets/today_training_card.dart
@@ -357,7 +357,7 @@ class _TodayTrainingRow extends ConsumerWidget {
                           fontFamily: 'JetBrainsMono',
                           fontSize: 10,
                           fontWeight: FontWeight.w700,
-                          color: programSessionColor(type),
+                          color: CrimpyTheme.textOn(programSessionColor(type)),
                         ),
                       ),
                       if (seconds > 0) ...[
@@ -459,8 +459,8 @@ class _FlexTrainingRow extends ConsumerWidget {
                 fontSize: 11,
                 fontWeight: FontWeight.w700,
                 color: done >= target
-                    ? CrimpyTheme.statusSuccess
-                    : programSessionColor(type),
+                    ? CrimpyTheme.textOn(CrimpyTheme.statusSuccess)
+                    : CrimpyTheme.textOn(programSessionColor(type)),
               ),
             ),
             const SizedBox(width: 10),
